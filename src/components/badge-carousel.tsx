d206666f870473606ebd239368b9f3c08e55b977
Oscar Persson <oscar.zhpersson@gmail.com>
2024-10-12 18:46:53 +0200

    feature/GH-4 Adds badge relevant component scripts

---
+++ src/components/badge-carousel.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export interface BadgeCarouselProps {
+  title: string
+  active?: boolean
+  image: {
+    id: string
+    url: string
+    alt: string
+  }
+}
+
+export function BadgeCarousel({ title, image, active }: BadgeCarouselProps) {
+  return (
+    <button
+      className={`flex flex-row justify-between items-center flex-shrink-0 snap-start
+                bg-white w-40 h-20 rounded-lg relative border-0.6 border-stroke
+                hover:bg-strokeOpaque hover:scale-95 transition-transform focus-visible:outline-none duration-50
+                ease-in-out ${active ? 'bg-strokeOpaque' : 'focus:bg-strokeOpaque focus:scale-95'}`}
+    >
+      <span
+        className="text-sm font-normal leading-none tracking-[-0.5px]
+                    absolute top-4 left-3"
+      >
+        {title}
+      </span>
+      <img
+        className="object-cover w-20 h-20 aspect-square
+                    translate-x-[5.55rem] absolute"
+        src={image.url}
+        alt={image.alt || title}
+      />
+    </button>
+  )
+}
