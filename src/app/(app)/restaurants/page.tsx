c5772fd6c716f5c95c050e8c964cecc8e7ca99b3
Oscar Persson <oscar.zhpersson@gmail.com>
2024-10-20 02:37:18 +0200

    feature/GH-5 Adds overlay data fetch from CMS to page

--- src/app/(app)/restaurants/page.tsx
+++ src/app/(app)/restaurants/page.tsx
@@ -4,11 +4,13 @@ import Image from 'next/image'
 import { FilterBadgeCarousel } from '@/components/filter-badge-carousel'
 import { RestaurantGrid } from '@/components/restaurant-grid'
 import { FilterMenu } from '@/components/filter-menu'
+import { AppOverlay } from '@/components/app-overlay'
 
 import { fetchPageData } from '@/services/fetchPageData'
 import { fetchRestaurantData } from '@/services/restaurantService'
 import { fetchLogo } from '@/services/fetchLogo'
 import { fetchDeliveryTimeRanges } from '@/services/fetchDeliveryRanges'
+import { fetchOverlayContent } from '@/services/fetchOverlayContent'
 
 import { formatNumberToDeliveryTimeRange } from '@/utils/formatNumberToDeliveryTimeRange'
 import { extractPriceRanges } from '@/utils/extractPriceRanges'
@@ -17,7 +19,7 @@ import type { RestaurantWithDetails } from '@/interfaces/restaurant'
 
 const Page = async () => {
   try {
-    const [page, restaurantData, logoUrl, deliveryTimeRanges] = await Promise.all([
+    const [page, restaurantData, logoUrl, deliveryTimeRanges, overlayContent] = await Promise.all([
       fetchPageData('restaurants').catch((err) => {
         throw new Error('Error fetching page data:', err)
       }),
@@ -29,6 +31,9 @@ const Page = async () => {
       }),
       fetchDeliveryTimeRanges().catch((err) => {
         throw new Error('Error fetching delivery time ranges:', err)
+      }),
+      fetchOverlayContent().catch((err) => {
+        throw new Error('Error fetching overlay content:', err)
       }),
     ])
 
@@ -52,24 +57,25 @@ const Page = async () => {
 
     return (
       <div className="w-full max-w-screen-displayMax mx-auto mb-6">
-        <header className="grid grid-cols-12 w-full gap-4 md:gap-0 px-8 lg:px-0">
+        <AppOverlay className="md:hidden" overlay={overlayContent} />
+        <header className="grid grid-cols-12 w-full gap-4 md:px-8 md:gap-0 lg:px-0">
           <Image
             src={logoUrl}
             alt="logo"
             width={275}
             height={40}
-            className="col-span-12 mt-11 min-w-[167px] w-[167px] h-10 md:w-[275px] md:h-[40px] md:my-11"
+            className="col-span-12 mx-8 md:mx-0 mt-11 min-w-[167px] w-[167px] h-10 md:w-[275px] md:h-[40px] md:my-11"
           />
-          <div className="col-span-12 md:col-span-3 lg:col-span-2 mb-4 md:mb-0">
+          <div className="col-span-12 md:col-span-3 lg:col-span-2 mb-4 mx-8 md:mx-0 md:mb-0">
             <FilterMenu
               filters={filters}
               deliveryTimeRanges={deliveryTimeRanges}
               priceRanges={priceRanges}
             />
           </div>
-          <main className="col-span-12 md:col-span-9 lg:col-span-10 md:ml-4">
+          <main className="col-span-12 md:col-span-9 lg:col-span-10 ml-8 md:ml-4">
             <FilterBadgeCarousel activeId={null} filters={filters} />
-            <div className="flex flex-col justify-between">
+            <div className="flex flex-col justify-between mr-8 md:mr-0">
               <h1 className="text-h1 md:text-display mt-6 mb-4 md:mt-11 md:mb-9">
                 {page.title || 'title'}
               </h1>
